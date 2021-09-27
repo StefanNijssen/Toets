04c7a7e601f56d0c2f1b8ae6edd3d0a4a4a2174f
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-27 14:20:33 +0200

    commit2

--- Toets.py
+++ Toets.py
@@ -3,25 +3,30 @@ Diploma = input("Heeft u een diploma in acteren?")
 Geslacht = input("Bent u man of vrouw? M/V")
 Lengte = int(input("Wat is uw lengte in centimeters?"))
 Gewicht = int(input("Wat is uw gewicht?"))
+Haar_kleur = input("Heeft u donker of blond haar?")
+
 
 def leeftijd_diploma():
     if Leeftijd >= 18 and Diploma == 'ja':
         return True
 
 def Man():
+    Snor = input("Heeft u een Snor?")
+    Kaal = input("Bent u kaal of bent u bereid kaal te gaan?")
+    Bril = input("Heeft u een bril?")
+    
+    groenenwoud = Leeftijd >= 60 and Kaal == "ja" and Bril == "nee" and Snor == "nee"
+    Van_Gelen = Leeftijd < 60 and Kaal == "nee" and Bril == "nee" or "ja" and Snor == "nee" and Haar_kleur == "donker"
+    
     if Leeftijd >= 60:
-        in
+        Groenenwoud()
+    elif leeftijd < 60:
+
 
 def Groenenwoud():
-    kaal = input("Bent u kaal of bent u bereid om kaal te gaan?")
-    if kaal == "ja":
-        if Lengte <= 165:
-            if Gewicht >= 90:
-                return True
-            else:
-                return False
-        else:
-            return False
-    else:
-        return False
-    
+    groenenwoud = kaal == "ja" and Lengte <= 165 and Gewicht >= 90
+
+if Geslacht == "M":
+    Man()
+else:
+    Vrouw()
